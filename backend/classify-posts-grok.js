0fdcad57b288966a59b23eb073543949e7e7da3b
Gabriel-xyz <gabrielschoonover2023@gmail.com>
2025-11-25 17:51:05 -0500

    removed coherent narrative

--- backend/classify-posts-grok.js
+++ backend/classify-posts-grok.js
@@ -121,7 +121,8 @@ export async function classifyPostsGrok(posts) {
     console.log('creating narrative summary failed')
   }
   let narrativeText = narrative ? narrative.choices[0].message.content.trim() : 'No response from server'
-  let fullText = 'AI Generated "Coherent Narrative":\n(Not as good as just reading the raw predictions below in my opinion. Can feel forced by the AI)\n\n' + narrativeText + '\n\n\n\nRaw Predictions:\n(May contain predictions not included in the "coherent narrative"):\n\n' + chunksText
+  // let fullText = 'AI Generated "Coherent Narrative":\n(Not as good as just reading the raw predictions below in my opinion. Can feel forced by the AI)\n\n' + narrativeText + '\n\n\n\nRaw Predictions:\n(May contain predictions not included in the "coherent narrative"):\n\n' + chunksText
+  let fullText = 'Predictions found on 4chan:\n\n' + chunksText
 
   fs.writeFileSync(path.join(__dirname, '..', 'data', 'reasoning.txt'), fullText);
 
